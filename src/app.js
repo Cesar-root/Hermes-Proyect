79d72da55a55268407f39e0bede96a1d90179f97
Koopa <15agostogalvan15@gmail.com>
2026-08-27 12:26:19 -0500

    Creacion de la configuracion (app) y del puerto (index)

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+
+
+const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('views', join(__dirname,'views'));
+app.set('view engine','ejs');
+
+
+export default app;
